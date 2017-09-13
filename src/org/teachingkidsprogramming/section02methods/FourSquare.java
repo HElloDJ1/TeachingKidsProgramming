3a5d308e2a6c6c39ef6e7c1e5d0a33ea62de30af
91085320 <91085320@mvusd.net>
2017-09-13 08:03:18 -0700

    Starting FourSquare.java

--- src/org/teachingkidsprogramming/section02methods/FourSquare.java
+++ src/org/teachingkidsprogramming/section02methods/FourSquare.java
@@ -1,17 +1,23 @@
 package org.teachingkidsprogramming.section02methods;
 
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+
 public class FourSquare
 {
   public static void main(String[] args)
   {
     //  Show the tortoise --#1
+    Tortoise.show();
     //  Make the tortoise move as fast as possible --#7
     //  Do the following 4 times --#8.1
     //      drawSquare (recipe below) --#6.1
     //      ------------- Recipe for drawSquare --#6.2
     //      Do the following 4 times --#5.1
     //          Change the pen color of the line the tortoise draws to a random color --#3
+    ColorWheel.getRandomColorFromWheel();
     //          Move the tortoise 50 pixels --#2
+    Tortoise.move(50);
     //          Turn the tortoise 90 degrees to the right --#4
     //      End Repeat --#5.2
     //      ------------- End of drawSquare recipe --#6.3
