98ad4b128c4d545b93a170c5c70f4286f739ab35
91085320 <91085320@mvusd.net>
2017-09-19 09:49:21 -0700

    Finishing java

--- src/org/teachingkidsprogramming/section02methods/PyramidsOfGiza.java
+++ src/org/teachingkidsprogramming/section02methods/PyramidsOfGiza.java
@@ -8,37 +8,21 @@ public class PyramidsOfGiza
   public static void main(String[] args) throws Exception
   {
     setUpPyramidLand();
-    //  Turn the tortoise 90 degrees to the left 
     Tortoise.turn(-90);
-    //  Move the tortoise 220 pixels
     Tortoise.move(220);
-    //  Turn the tortoise 135 degrees to the right
     Tortoise.turn(135);
-    //  Move the tortoise 100 pixels\
     Tortoise.move(100);
-    //  Turn the tortoise 90 degrees to the right
     Tortoise.turn(90);
-    //  Move the tortoise 100 pixels
     Tortoise.move(100);
-    //  Turn the tortoise 90 degrees to the left
     Tortoise.turn(-90);
-    //  Move the tortoise 100 pixels
     Tortoise.move(100);
-    //  Turn the tortoise 90 degrees to the right 
     Tortoise.turn(90);
-    //  Move the tortoise 100 pixels
     Tortoise.move(100);
-    //  Turn the tortoise 90 degrees to the left 
     Tortoise.turn(-90);
-    //  Move the tortoise 100 pixels
     Tortoise.move(100);
-    //  Turn the tortoise 90 degrees to the right 
     Tortoise.turn(90);
-    //  Move the tortoise 100 pixels
     Tortoise.move(100);
-    //  Turn the tortoise 135 degrees to the right 
     Tortoise.turn(135);
-    //  Move the tortoise 210 pixels\
     Tortoise.move(210);
   }
   private static void setUpPyramidLand()
