7278ad7ea0fad84f32d9c6a37782f67032915fcd
91085320 <91085320@mvusd.net>
2017-09-19 09:40:16 -0700

    Finishing Section 2

--- src/org/teachingkidsprogramming/section02methods/FourSquare.java
+++ src/org/teachingkidsprogramming/section02methods/FourSquare.java
@@ -9,17 +9,19 @@ public class FourSquare
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    //  Do the following 4 times --#8.1
-    //      drawSquare (recipe below) --#6.1
-    //      ------------- Recipe for drawSquare --#6.2
+    for (int i = 0; i < 4; i++)
+    {
+      drawSquare();
+      Tortoise.turn(90);
+    }
+  }
+  private static void drawSquare()
+  {
     for (int i = 0; i < 4; i++)
     {
       Tortoise.setPenColor(PenColors.getRandomColor());
       Tortoise.move(50);
       Tortoise.turn(90);
     }
-    //      ------------- End of drawSquare recipe --#6.3
-    //      Turn the tortoise 90 degrees to the right --#9
-    //  End Repeat --#8.2
   }
 }
