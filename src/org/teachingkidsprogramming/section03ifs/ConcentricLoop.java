a26fbc13b53b5211b1cc27ab47f669a706cb547c
91085320 <91085320@mvusd.net>
2017-10-09 10:13:15 -0700

    deep dive 02

--- src/org/teachingkidsprogramming/section03ifs/ConcentricLoop.java
+++ src/org/teachingkidsprogramming/section03ifs/ConcentricLoop.java
@@ -7,17 +7,13 @@ public class ConcentricLoop
   public static void main(String[] args)
   {
     Tortoise.show();
-    //  Set the tortoise x position to 150 --#4.1
     Tortoise.setX(150);
-    //  Set the tortoise y position to 200 --#4.2
     Tortoise.setY(200);
-    //  Set the tortoise speed to 10  --#4.3
     Tortoise.setSpeed(10);
     for (int i = 0; i < 360; i++)
     {
       Tortoise.move(3);
       Tortoise.turn(1);
-      //      If i is divisible by 20 --(HINT: Use mod operator '%') --#7
       if (i % 20 == 0)
       {
         for (int j = 0; j < 360; j++)
@@ -25,7 +21,6 @@ public class ConcentricLoop
           Tortoise.move(1);
           Tortoise.turn(1);
           Tortoise.setPenWidth(i / 100);
-          //          End Repeat --#8.2
         }
       }
     }
