69cc399ce21938ad654f0a54ef420c9e35e57810
91085320 <91085320@mvusd.net>
2017-10-31 10:00:43 -0700

    working on section 4

--- src/org/teachingkidsprogramming/section04mastery/DigiFlower.java
+++ src/org/teachingkidsprogramming/section04mastery/DigiFlower.java
@@ -2,6 +2,7 @@ package org.teachingkidsprogramming.section04mastery;
 
 import java.awt.Color;
 
+import org.teachingextensions.logo.Tortoise;
 import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
 import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
@@ -9,8 +10,9 @@ public class DigiFlower
 {
   public static void main(String[] args)
   {
-    //    Show the tortoise --#1
+    Tortoise.show();
     //    Make the tortoise move as fast as possible --#7
+    Tortoise.setSpeed(10);
     //    Make the background silver  --#8
     //    Make the line the tortoise draws 3 pixels wide --#15
     //    createColorPalette (recipe below) --#9.1
@@ -34,11 +36,12 @@ public class DigiFlower
     //    ------------- End of createColorPalette recipe --#9.3
     //
     //    ------------- Recipe for drawOctogon --#10.2
-    //    Do the following 8 times --#6.1
-    //      Change the pen color of the line the tortoise draws to the next color on the color wheel --#4
-    //      Move the tortoise 50 pixels --#2
-    //      Turn the tortoise 1/8th of 360 degrees to the right --#5
-    //    End Repeat --#6.2 
+    for (int i = 0; i < 8; i++)
+    {
+      Tortoise.setPenColor(ColorWheel.getNextColor());
+      Tortoise.move(50);
+      Tortoise.turn(360 / 8);
+    }
     //    ------------- End of drawOctogon recipe --#10.3
   }
 }
--- src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -1,44 +1,45 @@
 package org.teachingkidsprogramming.section04mastery;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class PentagonCrazy
 {
   public static void main(String[] args)
   {
     Tortoise.show();
-    //    Make the tortoise move as fast as possible --#3
     Tortoise.setSpeed(10);
-    //    createColorPalette (recipe below) --#8.1
+    createColorPalette();
     //
-    //    ------------- Recipe for createColorPalette --#8.2
-    //    Add steel blue to the color wheel --#7
-    //    Add dark orchid to the color wheel --#11
-    //    Add dark slate blue to the color wheel --#12
-    //    Add teal to the color wheel --#13
-    //    Add indigo to the color wheel --#14
-    //    ------------- End of createColorPalette recipe --#8.3
-    //
-    //    drawPentagon (recipe below) --#10.1
-    //
-    //    ------------- Recipe for drawPentagon --#10.2
+    drawPentagon();
+  }
+  private static void drawPentagon()
+  {
     for (int i = 0; i < 200; i++)
     {
-      //        adjustPen (recipe below) --#9.1
-      //
-      //        ------------- Recipe for adjustPen --#9.2
-      //        Change the pen color of the line the tortoise draws to the next color on the color wheel --#6
-      //        Increase the tortoises pen width by 1 --#15                                              
-      //        If the tortoise's pen width is greater than 4, then --#17
-      //            Reset the pen width to 1 --#16
-      //        ------------- End of adjustPen recipe --#9.3 
-      //
+      adjustPen();
       int Length = i;
       Tortoise.move(Length);
       Tortoise.turn(360 / 5);
-      //        Turn the tortoise 1 more degree --#5
       Tortoise.turn(1);
     }
-    //    ------------- End of drawPentagon recipe --#10.3
+  }
+  private static void adjustPen()
+  {
+    Tortoise.setPenColor(ColorWheel.getNextColor());
+    //        Increase the tortoises pen width by 1 --#15                                              
+    Tortoise.setPenWidth(+1);
+    //        If the tortoise's pen width is greater than 4, then --#17
+    //            Reset the pen width to 1 --#16
+    Tortoise.setPenWidth(1);
+  }
+  private static void createColorPalette()
+  {
+    ColorWheel.addColor(PenColors.Blues.SteelBlue);
+    ColorWheel.addColor(PenColors.Purples.DarkOrchid);
+    ColorWheel.addColor(PenColors.Blues.DarkSlateBlue);
+    ColorWheel.addColor(PenColors.Blues.Teal);
+    ColorWheel.addColor(PenColors.Purples.Indigo);
   }
 }
