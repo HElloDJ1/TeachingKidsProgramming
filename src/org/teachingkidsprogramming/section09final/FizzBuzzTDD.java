d62ecfa7f958ca264850063ac0e3cd994faaec33
91085320 <91085320@mvusd.net>
2017-12-07 14:28:29 -0800

    Finished FizzBuzz TDD

--- src/org/teachingkidsprogramming/section09final/FizzBuzzTDD.java
+++ src/org/teachingkidsprogramming/section09final/FizzBuzzTDD.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class FizzBuzzTDD
 {
-  //   For the numbers being tested, print out either that number, or, 
+  //   For the numbers being tested, print out either that number, or,
   //    If that number is divisible by 3, then print the word 'Fizz', 
   //    If that number is divisible by 5, then print the word 'Buzz', 
   //    If that number is divisible by 3 and 5, then print the word 'FizzBuzz'
@@ -14,11 +14,34 @@ public class FizzBuzzTDD
   //    Write tests using the Assert object via the TDD style
   //
   //    For more complete directions see this page
-  //    https://www.penflip.com/lynnlangit/tkp-lesson-plans/blob/master/course09.txt
   @Test
   public void test1Returns1()
   {
-    //String result = FizzBuzzTDD.convert(1);
-    assertEquals(1, 0);
+    String result = FizzBuzzTDD.convert(1);
+    assertEquals("1", result);
+  }
+  public static String convert(int num)
+  {
+    if (num % 15 == 0)
+      return "Fizz Buzz";
+    else if (num % 3 == 0)
+      return "Fizz";
+    return num + "";
+  }
+  @Test
+  public void test3ReturnsFizz()
+  {
+    String result = FizzBuzzTDD.convert(3);
+  }
+  @Test
+  public void test5ReturnsBuzz()
+  {
+    String result = FizzBuzzTDD.convert(5);
+  }
+  @Test
+  public void test15ReturnFizzBuzz()
+  {
+    String result = FizzBuzzTDD.convert(15);
+    assertEquals("Fizz Buzz", result);
   }
 }
