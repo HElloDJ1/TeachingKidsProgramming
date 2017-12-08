8404176cb1ffdb684b3728ff28906451d29e9857
91085320 <91085320@mvusd.net>
2017-12-08 13:31:07 -0800

    finished simpkeBubble

--- src/org/teachingkidsprogramming/section08events/SimpleBubble.java
+++ src/org/teachingkidsprogramming/section08events/SimpleBubble.java
@@ -1,35 +1,45 @@
 package org.teachingkidsprogramming.section08events;
 
+import java.util.Random;
+
+import org.teachingextensions.WindowUtils.ProgramWindow;
+import org.teachingextensions.logo.utils.ColorUtils.ColorWheel;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 import org.teachingextensions.logo.utils.EventUtils.MouseLeftClickListener;
+import org.teachingextensions.logo.utils.LineAndShapeUtils.Circle;
+import org.teachingextensions.virtualproctor.VirtualProctor;
 
 public class SimpleBubble implements MouseLeftClickListener
 {
+  ProgramWindow programWindow;
   public SimpleBubble()
   {
-    //    Create a ProgramWindow titled My Bubble --#1.1
-    //    Uncomment the line below -- #1.2
-    //    programWindow.setWindowVisible(true);
-    //    Have SimpleBubble listen for when the left mouse button is clicked in your program window --#2.2
-    //    prepareColorPalette (recipe below) --#7.1
-    //
-    //    ------------- Recipe for prepareColorPalette --#7.2
-    //         Add purple to the color wheel --#2.3
-    //         Add light steel blue to the color wheel --#4
-    //         Add blue to the color wheel --#5
-    //         Add dark blue to the color wheel --#6
-    //    ------------- End of prepareColorPalette recipe --#7.3
+    VirtualProctor.setName("Alexis L.");
+    programWindow = new ProgramWindow("My Bubble");
+    programWindow.setWindowVisible(true);
+    programWindow.addMouseLeftClickListener(this);
+    prepareColorPalette();
+  }
+  private void prepareColorPalette()
+  {
+    ColorWheel.addColor(PenColors.Purples.Purple);
+    ColorWheel.addColor(PenColors.Blues.LightBlue);
+    ColorWheel.addColor(PenColors.Blues.Blue);
+    ColorWheel.addColor(PenColors.Blues.DarkBlue);
   }
   @Override
   public void onLeftMouseClick(int x, int y)
   {
-    //     createBubble (recipe below) --#8.1
-    //     ------------- Recipe for createBubble --#8.2
-    //     Remove previous bubbles from your program window --#9
-    //     Set the radius for the circle to a random number between 10 and 50 --#2.5
-    //     Create a circle with the radius and the next color from the color wheel --#2.1
-    //     Move the center of the bubble to the current position of the mouse on the window --#3
-    //     Add the circle to your program window --#2.4
-    //    ------------- End of createBubble recipe --#8.3
+    createBubble(x, y);
+  }
+  private void createBubble(int x, int y)
+  {
+    // programWindow.clearWindow();
+    Random random = new Random();
+    int radius = random.nextInt(41) + 10;
+    Circle circle = new Circle(radius, ColorWheel.getNextColor());
+    circle.setCenter(x, y);
+    circle.addTo(programWindow);
   }
   public static void main(String[] args)
   {
